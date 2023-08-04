4535a3b30873be2fd16787c08069400c3beec2c7
Scott LeFoll <51521131+scottlefoll@users.noreply.github.com>
2023-08-04 14:21:13 -0400

    Update

--- app.py
+++ app.py
@@ -2,7 +2,24 @@ from flask import Flask
 from flask import jsonify
 app = Flask(__name__)
 
-def hello_json():
+@app.route('/home/')
+def home():
+    return "Home page"
+
+@app.route('/contact')
+def contact():
+    return "Contact page"
+
+@app.route('/hello/')
+def welcome():
+    return "Hello World!"
+
+@app.route('/numbers/<int:number>/')
+def print_list(number):
+    return jsonify(list(range(number)))
+
+@app.route('/persons/')
+def hello_persons():
     return ([
                 {'name':'Scott', 'country':'USA'},
                 {'name':'Maria', 'country':'Spain'},
@@ -16,28 +33,28 @@ def hello_json():
                 {'name':'Mei', 'country':'China'}
             ])
 
-@app.route('/home/')
-def home():
-    return "Home page"
-
-@app.route('/contact')
-def contact():
-    return "Contact page"
-
-@app.route('/hello/')
-def welcome():
-    return "Hello World!"
-
-@app.route('/numbers/')
-def print_list():
-    return jsonify(list(range(100)))
-
-@app.route('/persons/')
-def hello_person():
-    list = hello_json()
-
 @app.route('/person/<string:name>/')
+def hello_person(name):
+    people = [
+        {'name':'Scott', 'country':'USA'},
+        {'name':'Maria', 'country':'Spain'},
+        {'name':'Akiko', 'country':'Japan'},
+        {'name':'Lukas', 'country':'Germany'},
+        {'name':'Sophie', 'country':'France'},
+        {'name':'Giovanni', 'country':'Italy'},
+        {'name':'Sara', 'country':'Sweden'},
+        {'name':'Oliver', 'country':'United Kingdom'},
+        {'name':'Mikhail', 'country':'Russia'},
+        {'name':'Mei', 'country':'China'}
+    ]
 
+    # Filtering the list to find the record that matches the name (case-insensitive)
+    matching_person = next((person for person in people if person['name'].lower() == name.lower()), None)
+
+    if matching_person is not None:
+        return jsonify(matching_person)
+    else:
+        return jsonify({"error": "Person not found"}), 404
 
 @app.route('/<int:number>/')
 def incrementer(number):
